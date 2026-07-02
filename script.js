c435171eb72759357a64c435dbc8123ace24d0c5
Samuel Santini <samuel.santini.fernandes@gmail.com>
2026-07-02 00:09:02 -0300

    adicionando funcoes com JS no projeto

---
+++ script.js
@@ -0,0 +1,20 @@
+function mudarfotoSelecao() {
+  document.body.style
+    .backgroundImage = "url('fotos/selecao-brasil.jpg')";
+}
+
+function mudarfotoLogo() {
+  document.body.style
+    .backgroundImage = "url('fotos/imagem-logo.png')";
+}
+
+function mudarfotoEscudo() {
+  document.body.style
+    .backgroundImage = "url('fotos/escudo-selecao.png')";
+}
+
+function voltarFotoOriginal() {
+  document.body.style
+    .backgroundImage = "url('fotos/logo-CBF.jpg')";
+}
+
